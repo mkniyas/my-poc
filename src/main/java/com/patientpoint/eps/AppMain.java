5eaf6340759755bae9fb22b8fcec6e5517dfe05c
Niyas <122938174+mkniyas@users.noreply.github.com>
2025-10-07 09:48:01 -0500

    Added src/main/java/com/patientpoint/eps/AppMain.java

---
+++ src/main/java/com/patientpoint/eps/AppMain.java
@@ -0,0 +1,14 @@
+package com.patientpoint.eps;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+public class AppMain {
+  public static void main(String[] args) { SpringApplication.run(AppMain.class, args); }
+}
+@RestController
+class HelloController {
+  @GetMapping("/")
+  String hello() { return "Editorial PUblisher is alive!"; }
+}
